498fc6bcffb85fd42284f9e6990fdbe1cb0fe78a
Astrum <potatoedtophat@outlook.com>
2022-05-07 19:13:44 -0500

    Added MudBlazor and initial code for dark mode & seizure dialog.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 using Website;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
+
